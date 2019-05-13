2a57afd89e668c01a42f53e4efccb8e5be9c13a0
Erik Behnke <erik.behnke@gmail.com>
2019-05-13 09:32:32 -0500

    able to post times to table

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,18 +1,34 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
+import com.codeup.blog.models.User;
+import com.codeup.blog.repositories.PostRepository;
+import com.codeup.blog.repositories.UserRepository;
+import com.codeup.blog.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.Time;
+import java.util.Date;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String welcome2() {
-        return "home";
+    private final PostRepository postsRepo;
+    private final UserRepository usersRepo;
+//    private EmailService emailService;
+
+    public HomeController(PostRepository postsRepo, UserRepository usersRepo) {
+        this.postsRepo = postsRepo;
+        this.usersRepo = usersRepo;
     }
-    @GetMapping("/home")
+
+    @GetMapping("/")
     public String welcome() {
         return "home";
     }
@@ -21,5 +37,31 @@ public class HomeController {
     public String welcome(@PathVariable String name, Model viewModel) {
         viewModel.addAttribute("name", name);
         return "home";
+    }
+
+    @GetMapping("/home")
+    public String welcome2(Model model) {
+        model.addAttribute("datetime", new Post());
+        return "home";
+    }
+
+    @PostMapping("/home")
+    public String datepicker(@ModelAttribute Post postToSaved){
+        User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userDB = usersRepo.findOne(sessionUser.getId());
+        postToSaved.setAuthor(userDB);
+        Post savedPost = postsRepo.save(postToSaved);
+
+
+
+
+//        model.addAttribute("datepicker1", datepicker1);
+//        model.addAttribute("timepicker1", timepicker1);
+//        model.addAttribute("datepicker2", datepicker2);
+//        model.addAttribute("timepicker2", timepicker2);
+//        System.out.println("" + datepicker1 + timepicker1 + datepicker2 + timepicker2);
+//        return "home";
+        return "redirect:/posts/" + savedPost.getId();
+
     }
 }
